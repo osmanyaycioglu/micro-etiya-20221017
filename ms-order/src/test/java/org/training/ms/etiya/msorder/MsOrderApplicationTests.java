796a8d0f595e8f41be6a221c53bee7fe37c2841c
osman <osman@osy.com>
2022-10-18 11:22:57 +0300

    day 2 2

---
+++ ms-order/src/test/java/org/training/ms/etiya/msorder/MsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.ms.etiya.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
