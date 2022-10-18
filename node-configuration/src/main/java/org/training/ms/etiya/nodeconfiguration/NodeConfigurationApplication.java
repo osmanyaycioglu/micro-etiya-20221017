8c074270c654f2e4cef7e5dd82651629ee514b0f
osman <osman@osy.com>
2022-10-18 10:55:19 +0300

    day 2 1

--- node-configuration/src/main/java/org/training/ms/etiya/nodeconfiguration/NodeConfigurationApplication.java
+++ node-configuration/src/main/java/org/training/ms/etiya/nodeconfiguration/NodeConfigurationApplication.java
@@ -2,8 +2,10 @@ package org.training.ms.etiya.nodeconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class NodeConfigurationApplication {
 
     public static void main(String[] args) {
